2ceef4119eb7e39e022a40c626f965313dfd0a3f
hudzaiflank <hudzaifah.afif35@gmail.com>
2025-10-15 20:06:15 +0700

    last

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/lomba-rasya/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
 });
